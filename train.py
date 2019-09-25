f099ae26b1d859c33169035e434e519d9cb3b66d
Maurice Verbrugge <maurice.verbrugge@gmail.com>
2019-09-25 10:40:50 +0200

    added train labels etc

--- train.py
+++ train.py
@@ -6,11 +6,20 @@ import import_data
 
 
 trainX = import_data.loadX("data/train-images-idx3-ubyte.gz")
-trainY = import_data.loadY("data/train-labels-idx1-ubyte.gz")
+trainLabel = import_data.loadY("data/train-labels-idx1-ubyte.gz")
 testX = import_data.loadX("data/t10k-images-idx3-ubyte.gz")
-testY = import_data.loadY("data/t10k-labels-idx1-ubyte.gz")
+testLabel = import_data.loadY("data/t10k-labels-idx1-ubyte.gz")
 
+)
 # Select 3 and 7 numbers X,Y
+trainLabelT = np.zeros(len(trainLabel))
+trainLabelS = np.zeros(len(trainLabel)
+for i in len(trainLabel):
+    if trainLabel[i] == 3:
+        trainLabelT[i] = 1
+    if trainLabel[i] == 7:
+        trainLabelS[i] = 1
+
 
 for i in methods:
         #fit, for different methods, record time
