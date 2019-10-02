363ca1cdd388dce19adfac68728f2bd7dbc2f86d
MauiGonzo <maurice.verbrugge@gmail.com>
2019-10-02 12:06:10 +0200

    minors

--- train.py
+++ train.py
@@ -36,7 +36,8 @@ trainLabel_t = np.array(trainLabel_t)
 model = perceptron.Perceptron(len(trainX_t[0]))
 
 #E = model.fit(trainX_t, trainLabel_t, learning = 'gradient', epochs=100)
-E = model.fit(trainX_t, trainLabel_t, learning = 'momentum', epochs=100)
+#E = model.fit(trainX_t, trainLabel_t, learning = 'momentum', epochs=100)
+E = model.fit(trainX_t, trainLabel_t, learning = 'decay', epochs=100)
 
 plt.plot(range(len(E)), E)
 plt.title('Lowest cost: {}'.format(min(E)))
