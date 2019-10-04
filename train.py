c8ff02d32b59b85601426b70f78974d572c34552
Remco Volmer <rvolmer@valburg.science.ru.nl>
2019-10-04 10:47:26 +0200

    Meer methodes fitten

--- train.py
+++ train.py
@@ -38,24 +38,13 @@ for i in range(len(testLabel)):
 trainX_t = np.array(trainX_t)/np.amax(trainX_t)
 trainLabel_t = np.array(trainLabel_t)
 
-<<<<<<< HEAD
 testX_t = np.array(testX_t)/np.amax(testX_t)
 testLabel_t = np.array(testLabel_t)
 
-
-'Make models'
-model_gradient = perceptron.Perceptron(len(trainX_t[0]))
-model_momentum = perceptron.Perceptron(len(trainX_t[0]))
-model_decay = perceptron.Perceptron(len(trainX_t[0]))
-model_newton = perceptron.Perceptron(len(trainX_t[0]))
-model_line = perceptron.Perceptron(len(trainX_t[0]))
-model_conjugate = perceptron.Perceptron(len(trainX_t[0]))
-
-=======
 def grad_algo_testing(learing_type, nr_epochs):
      model = perceptron.Perceptron(len(trainX_t[0]))
      t1_start = perf_counter()
-     E = model.fit(trainX_t, trainLabel_t, learning = learing_type, epochs=nr_epochs)
+     E = model.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = learing_type, epochs=nr_epochs)
      t1_stop = perf_counter()
      print("Running time for", learing_type, " = ", t1_stop - t1_start, "seconds" )
      print("results in E_training: ", E)
@@ -71,34 +60,33 @@ def grad_algo_testing(learing_type, nr_epochs):
 grad_algo_testing('gradient', 10)
 #grad_algo_testing('momentum', 10)
 #grad_algo_testing('decay',    10)
->>>>>>> dd29b935ad958612bf01701633888f31f4d50b53
-
-print('Training gradient:')
-E_train_gradient, E_test_gradient = model_gradient.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = 'gradient', epochs=10)
-
-print('Training momentum:')
-E_train_momentum, E_test_momentum = model_momentum.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = 'momentum', epochs=10)
-
-print('Training decay:')
-E_train_decay, E_test_decay = model_decay.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = 'decay', epochs=10)
-
-
-'Make plots'
-fig, axs = plt.subplots(2, 2)
-
-axs[0,0].plot(range(len(E_train_gradient)), E_train_gradient, label='Train')
-axs[0,0].plot(range(len(E_test_gradient)), E_test_gradient, label='Test')
-axs[0,0].set_title('Gradient: Lowest cost train: {}, lowest cost test: {}'.format(np.round(min(E_train_gradient), 4), np.round(min(E_test_gradient), 4)))
-axs[0,0].legend()
-
-
-axs[0,1].plot(range(len(E_train_momentum)), E_train_momentum, label='Train')
-axs[0,1].plot(range(len(E_test_momentum)), E_test_momentum, label='Test')
-axs[0,1].set_title('Momentum: Lowest cost train: {}, lowest cost test: {}'.format(np.round(min(E_train_momentum), 4), np.round(min(E_test_momentum), 4)))
-axs[0,1].legend()
-
-axs[1,0].plot(range(len(E_train_decay)), E_train_decay, label='Train')
-axs[1,0].plot(range(len(E_test_decay)), E_test_decay, label='Test')
-axs[1,0].set_title('Decay: Lowest cost train: {}, lowest cost test: {}'.format(np.round(min(E_train_decay), 4), np.round(min(E_test_decay), 4)))
-axs[1,0].legend()
-plt.show()
+#
+# print('Training gradient:')
+# E_train_gradient, E_test_gradient = model_gradient.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = 'gradient', epochs=10)
+#
+# print('Training momentum:')
+# E_train_momentum, E_test_momentum = model_momentum.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = 'momentum', epochs=10)
+#
+# print('Training decay:')
+# E_train_decay, E_test_decay = model_decay.fit(trainX_t, trainLabel_t, testX_t, testLabel_t, learning = 'decay', epochs=10)
+#
+#
+# 'Make plots'
+# fig, axs = plt.subplots(2, 2)
+#
+# axs[0,0].plot(range(len(E_train_gradient)), E_train_gradient, label='Train')
+# axs[0,0].plot(range(len(E_test_gradient)), E_test_gradient, label='Test')
+# axs[0,0].set_title('Gradient: Lowest cost train: {}, lowest cost test: {}'.format(np.round(min(E_train_gradient), 4), np.round(min(E_test_gradient), 4)))
+# axs[0,0].legend()
+#
+#
+# axs[0,1].plot(range(len(E_train_momentum)), E_train_momentum, label='Train')
+# axs[0,1].plot(range(len(E_test_momentum)), E_test_momentum, label='Test')
+# axs[0,1].set_title('Momentum: Lowest cost train: {}, lowest cost test: {}'.format(np.round(min(E_train_momentum), 4), np.round(min(E_test_momentum), 4)))
+# axs[0,1].legend()
+#
+# axs[1,0].plot(range(len(E_train_decay)), E_train_decay, label='Train')
+# axs[1,0].plot(range(len(E_test_decay)), E_test_decay, label='Test')
+# axs[1,0].set_title('Decay: Lowest cost train: {}, lowest cost test: {}'.format(np.round(min(E_train_decay), 4), np.round(min(E_test_decay), 4)))
+# axs[1,0].legend()
+# plt.show()
