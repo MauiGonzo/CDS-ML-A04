08328eae400a974d89d578515c47e5c7b331e4c8
Maurice Verbrugge <mauriceverbrugge@MacBook-Pro-van-Maurice.local>
2019-10-06 14:45:03 +0200

    edit decay

--- perceptron.py
+++ perceptron.py
@@ -107,17 +107,14 @@ class Perceptron():
         learning_rate = 1
         alpha = 0.1
         # A) use rough approximation method to calculate gradient with momentum 
-        #delta_w = - learning_rate/(1 - alpha) * self.__gradient(x, y, t)
+        # delta_w = - learning_rate/(1 - alpha) * self.__gradient(x, y, t)
         # B) add delta-w-min-1 to gradient
-        #if 'self.delta_w_min_one' not in globals():
         if hasattr(self, 'delta_w_min_one' ):
-            #delta_w_min_one has been created
+            # delta_w_min_one has been created
             delta_w = - learning_rate * self.__gradient(x, y, t) + alpha * self.delta_w_min_one
         else:
-            #else delta_w_min_one, has not been created
-            delta_w = - learning_rate * self.__gradient(x, y, t) # the delta_w_min_one-part during first epoch =0
-            self.delta_w_min_one = np.copy(delta_w)
-
+            # else delta_w_min_one, has not been created
+            delta_w = - learning_rate * self.__gradient(x, y, t)       # the delta_w_min_one-part during first epoch =0
         self.delta_w_min_one = np.copy(delta_w)
         # return delta_weights
         return delta_w
@@ -125,10 +122,8 @@ class Perceptron():
     def __learning_decay(self, x, y, t):
         # now add weight decay term and use the momentum learning method
         learning_rate = 1
-        # choose momentum factor alpha - assignment says you have to try values
-        alpha = 0.1
         # use approximation method to calculate gradient with momentum
-        delta_w = - learning_rate/(1 - alpha) * self.__gradient(x, y, t) + alpha * self.__gradient(x, y, t)
+        delta_w = - learning_rate *  self.__gradient(x, y, t)
         # return delta_weights
         return delta_w
 
